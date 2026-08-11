76072b07917e5a48b0fefce45cb21f7b471d1efb
Vricap <vricagedepenggalih@gmail.com>
2026-08-11 15:10:12 +0700

    feat: basic property table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Property;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    $properties = Property::all();
+
+    return view('dashboard', ['properties' => $properties]);
 });
